28ec3af5407649bdd7663364d575d99b7ec91489
Meo73preb <meophuc2011@gmail.com>
2025-08-13 10:43:50 +0700

    Create Key.lua

---
+++ Key.lua
@@ -0,0 +1,24 @@
+-- Danh sách key hợp lệ
+local validKeys = {
+    ["key_75088"] = true,
+    ["key_test"] = true,
+    ["key nhập ở đây"] = false
+}
+
+-- Lấy key từ getgenv
+local inputKey = tostring(getgenv().Key or "")
+
+-- Kiểm tra key
+if validKeys[inputKey] then
+    print("✅ Key đúng! Chạy script...")
+    -- CODE CHÍNH CỦA BẠN VIẾT Ở DƯỚI 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Meo73preb/Test/refs/heads/main/testchest-bf.lua"))()
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Thông báo";
+        Text = "Chào mừng bạn vào script VIP!";
+        Duration = 5;
+    })
+else
+    warn("❌ Key sai! Không thể chạy script.")
+    return
+end
