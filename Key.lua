7a1740ecb07422af38e5bb5ca5402c4a8443d5b4
Meo73preb <meophuc2011@gmail.com>
2025-08-16 09:06:49 +0700

    Key.lua

--- Key.lua
+++ Key.lua
@@ -1,17 +1,13 @@
--- Danh sách key hợp lệ
 local validKeys = {
     ["key_75088"] = true,
     ["key_test"] = true,
     ["key nhập ở đây"] = false
 }
 
--- Lấy key từ getgenv
 local inputKey = tostring(getgenv().Key or "")
 
--- Kiểm tra key
 if validKeys[inputKey] then
-    print("✅ Key đúng! Chạy script...")
-    -- CODE CHÍNH CỦA BẠN VIẾT Ở DƯỚI 
+    print("✅ Key đúng! Đang Chạy script...") 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Meo73preb/Test/refs/heads/main/testchest-bf.lua"))()
     game.StarterGui:SetCore("SendNotification", {
         Title = "Thông báo";
